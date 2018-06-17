438eb01dff619a971d5dd9b78772dfeb6cc8e7ac
NEPAL <nabin@Nabins-MacBook-Air.local>
2018-06-17 15:37:03 +0545

    eror

--- notes/admin.py
+++ notes/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models there.
 # Worldcup is going to win by Germany
 from .models import Note
- 
+
 class NoteAdmin(admin.ModelAdmin):
     class Meta:
         model = Note
